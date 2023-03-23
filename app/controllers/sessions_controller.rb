cde1bfa94775f76ae4a1e79e1d203ca9988e0164
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-03-23 19:30:28 +0300

    Create function and user_login integration test

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # login the user and direct to users show page
+      reset_session #reset a session immediately before logging in so that the attackers desired is get cleared and freshly created id ends in session hash
+      log_in user
+      redirect_to user
+    else
+      # create an error message
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  
+end
